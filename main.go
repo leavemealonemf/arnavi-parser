b1b2f37bd1dbdae525ed5e0b0df3a42e7d3aefee
leavemealonemf <strangemisterio78@gmail.com>
2025-02-08 21:31:06 +0300

    upd

--- main.go
+++ main.go
@@ -177,6 +177,7 @@ func handleServe(conn net.Conn) {
 			// 	PacketDataLen: hexPackageData[6:8],
 			// 	Unixtime:      hexPackageData[8:16],
 			// }
+			fmt.Println("origin len:", hexPackageData[6:8])
 			fmt.Println("reversed len:", reverseBytes(hexPackageData[6:8]))
 
 			dataLenBytes := hexToDec(BytesToHexString(reverseBytes(hexPackageData[6:8])))
