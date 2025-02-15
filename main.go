8126672c22e46447edde880e5c673d869cb32860
leavemealonemf <strangemisterio78@gmail.com>
2025-02-15 14:31:23 +0300

    rabbit init

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -739,6 +740,46 @@ func initIOTCommands() {
 	}
 }
 
+func RabbitMQInit() {
+	conn, err := amqp.Dial("amqp://vano:123@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
 var scooterColl *mongo.Collection
 var cmdsColl *mongo.Collection
 var ctx = context.TODO()
@@ -750,6 +791,7 @@ func init() {
 }
 
 func main() {
+	RabbitMQInit()
 	mongUsr, _ := os.LookupEnv("MONGO_USR")
 	mongPass, _ := os.LookupEnv("MONGO_PASSWORD")
 	mongHost, _ := os.LookupEnv("MONGO_HOST")
