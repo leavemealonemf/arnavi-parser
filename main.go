32c9e0f22c53b8bb6bb0b397f22f2a1463593a9e
leavemealonemf <strangemisterio78@gmail.com>
2025-02-21 14:58:09 +0300

    upd

--- main.go
+++ main.go
@@ -544,7 +544,7 @@ func AcceptCommand(rc *ReceivedCommand) {
 	r, _ := json.Marshal(rc)
 	err := rbtChannel.Publish(
 		"",
-		"",
+		rc.QueueD.RoutingKey,
 		false,
 		false,
 		amqp.Publishing{
@@ -589,7 +589,7 @@ func WaitCommands() {
 					msg := fmt.Sprintf("this command does not exist %s", cmd)
 					err = rbtChannel.Publish(
 						"",
-						"",
+						d.RoutingKey,
 						false,
 						false,
 						amqp.Publishing{
@@ -632,7 +632,7 @@ func WaitCommands() {
 				msg := fmt.Sprintf("device with imei %s not connected", imei)
 				err = rbtChannel.Publish(
 					"",
-					"",
+					d.RoutingKey,
 					false,
 					false,
 					amqp.Publishing{
